64e0155677eea08cfd28bea5b6a376b9619c4ec0
Andrey Reno <oni.river@gmail.com>
2025-09-16 22:48:24 +0300

    added Input

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,15 +1,17 @@
 import { Button } from '@/shared/ui/Button';
 import Link from 'next/link';
+import { Input } from '@/shared/ui/Input';
 
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center">
             <h1>Это каталожная страница</h1>
-            <div className={'flex gap-2'}>
-                <Button variant="default" asChild>
-                    <Link href="/">На главную (ссылка как кнопка)</Link>
-                </Button>
+            <Button variant="default" asChild>
+                <Link href="/">На главную (ссылка как кнопка)</Link>
+            </Button>
+            <div className={'mt-2 flex gap-2'}>
                 <Button variant={'destructive'}>Кнопка</Button>
+                <Input type={'tel'} />
             </div>
         </div>
     );
