d3d81cab467199399265987c9098125e46ce9a31
Andrey-Reno <oni.river@gmail.com>
2025-09-20 09:19:51 +0300

    refactor: split sticky behaviour into hook, behaviour provider and header ui

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -3,30 +3,34 @@ import { HeaderButton } from './ui/HeaderButton';
 import { Logo } from './ui/Logo';
 import Link from 'next/link';
 import { Search, ShoppingBasket } from 'lucide-react';
-import { HidingBehaviourProvider } from '@/widgets/Header/ui/HidingBehaviour';
+import { ScrollVisibilityClient } from '@/widgets/Header/ScrollVisibilityClient';
+import { cn } from '@/shared/lib/cn';
 
 export const Header = () => {
-  return (
-    <header className="sticky top-0 z-10">
-      <HidingBehaviourProvider>
-        <div className="bg-background container flex h-14 items-center justify-between drop-shadow-xs">
-          <div id="left-buttons" className="flex h-full">
-            <BurgerMenu />
-            <Logo />
-          </div>
+  const headerClasses = cn(
+    'bg-background container flex h-14 items-center justify-between drop-shadow-xs',
+    'sticky top-0 z-10',
+    'translate-y-0 transition-transform duration-300',
+  );
 
-          <div id="right-buttons" className="h-full">
-            <HeaderButton variant="ghost" className="w-14 border-l-1">
-              <Search className="text-foreground size-7" />
-            </HeaderButton>
-            <HeaderButton variant="ghost" className="w-14 border-l-1" asChild>
-              <Link href="/cart">
-                <ShoppingBasket className="text-foreground size-7" />
-              </Link>
-            </HeaderButton>
-          </div>
-        </div>
-      </HidingBehaviourProvider>
+  return (
+    <header ref={undefined} data-smart-sticky className={headerClasses}>
+      <div id="left-buttons" className="flex h-full">
+        <BurgerMenu />
+        <Logo />
+      </div>
+
+      <div id="right-buttons" className="h-full">
+        <HeaderButton variant="ghost" className="w-14 border-l-1">
+          <Search className="text-foreground size-7" />
+        </HeaderButton>
+        <HeaderButton variant="ghost" className="w-14 border-l-1" asChild>
+          <Link href="/cart">
+            <ShoppingBasket className="text-foreground size-7" />
+          </Link>
+        </HeaderButton>
+      </div>
+      <ScrollVisibilityClient />
     </header>
   );
 };
